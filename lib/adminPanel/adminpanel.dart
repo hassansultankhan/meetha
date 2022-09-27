00c49fd74c991aff27100c1fc5b8dc800d4b18e7
Hassan Sultan Khan <hassansultankhan@gmail.com>
2022-09-27 12:13:29 -0700

    27/9/22

--- lib/adminPanel/adminpanel.dart
+++ lib/adminPanel/adminpanel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meetha/productClass.dart';
 
 class adminPanel extends StatefulWidget {
   const adminPanel({Key? key}) : super(key: key);
@@ -14,9 +15,156 @@ class _adminPanelState extends State<adminPanel> {
   final TextEditingController vendorLocationController =
       TextEditingController();
   final TextEditingController priceController = TextEditingController();
+  final TextEditingController sizeController = TextEditingController();
+
+  final CollectionReference _products =
+      FirebaseFirestore.instance.collection('Details');
+
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(labelText: 'TITLE'),
+                ),
+                TextField(
+                  controller: vendorController,
+                  decoration: const InputDecoration(labelText: 'VENDOR'),
+                ),
+                TextField(
+                  controller: vendorLocationController,
+                  decoration:
+                      const InputDecoration(labelText: 'VENDOR LOCATION'),
+                ),
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  controller: priceController,
+                  decoration: const InputDecoration(labelText: 'PRICE'),
+                ),
+                TextField(
+                  keyboardType:
+                      const TextInputType.numberWithOptions(decimal: true),
+                  controller: sizeController,
+                  decoration: const InputDecoration(labelText: 'SIZE'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      final String title = titleController.text;
+                      final String vendor = vendorController.text;
+                      final String vendorLocation = vendorController.text;
+                      final double? price =
+                          double.tryParse(priceController.text);
+                      if (price != null) {
+                        await _products.add({
+                          "title": title,
+                          "vendor": vendor,
+                          "vendorLocation": vendorLocationController,
+                          "price": priceController,
+                          "size": sizeController,
+                        });
+
+                        titleController.text = "";
+                        vendorController.text = "";
+                        vendorLocationController.text = "";
+                        priceController.text = "";
+                        sizeController.text = "";
+
+                        Navigator.of(context).pop();
+                      }
+                    },
+                    child: const Text("Add product")),
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+      titleController.text = documentSnapshot['title'];
+      vendorController.text = documentSnapshot['vendor'];
+      vendorLocationController.text = documentSnapshot['vendorLocation'];
+      priceController.text = documentSnapshot['price'].toString();
+      sizeController.text = documentSnapshot['size'].toString();
+    }
+
+    await showModalBottomSheet(
+        context: context,
+        builder: (BuildContext ctx) {
+          // to be continued here
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meetha Admin'),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+          stream: _products.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                  itemCount: streamSnapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final DocumentSnapshot documentSnapshot =
+                        streamSnapshot.data!.docs[index];
+                    return Card(
+                      margin: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Text(documentSnapshot['name']),
+                          Text(documentSnapshot['vendor']),
+                          Text(documentSnapshot['vendorLocation']),
+                          Text(documentSnapshot['price']),
+                          Text(documentSnapshot['size']),
+                          Row(
+                            children: [
+                              IconButton(
+                                  onPressed: () {
+                                    _update(documentSnapshot);
+                                  },
+                                  icon: const Icon(Icons.edit)),
+                              IconButton(
+                                  onPressed: () {
+                                    _delete(documentSnapshot.id);
+                                  },
+                                  icon: const Icon(Icons.delete)),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 2,
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _create(),
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
